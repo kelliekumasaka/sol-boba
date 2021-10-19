6a949b2153cf6cfb7a1ba7e2f5d18ff6eb646eb7
kelliekumasaka <kelliek3@gmail.com>
2021-10-18 17:42:52 -0700

    menu rearragement and sign js

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,25 @@
+const rotateSign = () => {
+    console.log('hello')
+}
+
 if (moment().format("dddd")==="Thursday" && moment().hour()>=10 && moment().hour()<17|| moment().format("dddd") ==="Friday" && moment().hour()>=10 && moment().hour()<17 || moment().format("dddd") ==="Monday" && moment().hour()>=10 && moment().hour()<17 || moment().format("dddd") ==="Tuesday" && moment().hour()>=10 && moment().hour()<17 || moment().format("dddd") ==="Wednesday" && moment().hour()>=10 && moment().hour()<17 || moment().format("dddd") ==="Saturday" && moment().hour()>=10 && moment().hour()<19 || moment().format("dddd") ==="Sunday" && moment().hour()>=10 && moment().hour()<19){
     console.log('we are open!');
-    const openSign = document.createElement("p");
-    openSign.textContent = "We are now open !!";
-    document.querySelector("#hours").append(openSign);
+    const openSign = document.createElement("img");
+    openSign.setAttribute("src","assets/images/PikPng.com_open-sign-png_659101.png");
+    openSign.setAttribute("width", "40%");
+    // openSign.setAttribute("opacity", "0.5");
+    document.querySelector("#currentStatus").append(openSign);
+    openSign.setAttribute("id","openSign");
+    openSign.addEventListener("mouseover", rotateSign());
 }else{
     console.log('we are closed :(');
-    const closedSign = document.createElement("p");
-    closedSign.textContent = "We are now closed :(";
-    document.querySelector("#hours").append(closedSign);
+    const closedSign = document.createElement("img");
+    closedSign.setAttribute("src","assets/images/PikPng.com_leaf-vinayagar-png_5052591.png");
+    closedSign.setAttribute("width", "40%");
+    // closedSign.setAttribute("opacity", "0.5");
+    document.querySelector("#currentStatus").append(closedSign);
+    closedSign.setAttribute("id", "closedSign");
+    closedSign.addEventListener("mouseover", rotateSign());
+
 }
 
